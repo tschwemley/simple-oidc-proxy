ad02727df7d95e731789cab2b69919d2f42a4e60
Tyler Schwemley <tjschwem@gmail.com>
2024-10-05 00:09:24 -0400

    fix build and re-add env file (encrypted)

--- flake.nix
+++ flake.nix
@@ -33,10 +33,12 @@
 
           devShells.default = pkgs.mkShell {
             buildInputs = with pkgs; [
+              age
               air
               go
               nodejs
               sqlc
+              sops
               tailwindcss
               templ
             ];
@@ -47,14 +49,44 @@
           };
 
           packages.default = pkgs.buildGo123Module {
-            # __noChroot = true;
+            #__noChroot = true;
 
-            name = "oidc-sso";
+            name = "oidcsso";
 
             src = ./.;
 
             vendorHash = "sha256-/y4upXlHhq3Vuz5TgKxI+L35+aMoQVlu2A9OfzoyOx0=";
-            # preferLocal = true;
+            preferLocal = true;
+
+            # impureEnvVars = [
+            #   "REPO_HOST"
+            #   "REPO_USER"
+            #   "REPO_PASS"
+            # ];
+
+            preBuild = # bash
+              ''
+                export HOME=$(mktemp -d)
+                export NIX_SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt
+                export SOPS_AGE_KEY_FILE=/etc/sops/age-keys.txt
+
+                source <(${pkgs.sops}/bin/sops -d ${./build.env})
+                echo $REPO_HOST
+
+                mkdir -p ~/.ssh/
+
+                cat > ~/.ssh/known_hosts <<EOF
+                git.schwem.io ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMjkwVwr1iP35frNmaaB+eW63+m8ExKtQ5OA8qen4dz0
+                EOF
+
+                cat > ~/.netrc <<EOF
+                  machine $REPO_HOST
+                    login $REPO_USER
+                    password $REPO_PASS
+                EOF
+              '';
+
+            GOPRIVATE = "git.schwem.io";
           };
         };
     };
