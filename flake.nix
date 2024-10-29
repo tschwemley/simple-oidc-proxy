0cce6e066b30c4b113d83c2e0242c78bfacceb87
Tyler Schwemley <tjschwem@gmail.com>
2024-10-29 02:31:48 -0400

    update flake

--- flake.nix
+++ flake.nix
@@ -6,77 +6,77 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs =
-    inputs@{
-      flake-parts,
-      nixpkgs,
-      ...
-    }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
+  outputs = inputs @ {
+    flake-parts,
+    nixpkgs,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
       systems = [
         "x86_64-linux"
       ];
 
-      perSystem =
-        {
-          config,
-          pkgs,
-          system,
-          ...
-        }:
-        {
-          # makes pkgs available to all perSystem functions
-          _module.args.pkgs = import nixpkgs {
-            inherit system;
-            config.allowUnfree = true;
-          };
-
-          devShells.default = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              age
-              air
-              go
-              nodejs
-              sqlc
-              sops
-              tailwindcss
-              templ
-            ];
-
-            shellHook = ''
-              export GOPRIVATE=git.schwem.io
-            '';
-          };
-
-          packages.default = pkgs.buildGo123Module {
-            name = "oidcsso";
-
-            src = ./.;
-            vendorHash = "sha256-06buojNRtqvr/uBc/572smG92UfwSSVlQlLZkULwDXo=";
-            preBuild = # bash
-              ''
-                export HOME=$(mktemp -d)
-                export NIX_SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt
-                export SOPS_AGE_KEY_FILE=/etc/sops/age-keys.txt
-
-                source <(${pkgs.sops}/bin/sops -d ${./build.env})
-                echo $REPO_HOST
-
-                mkdir -p ~/.ssh/
-
-                cat > ~/.ssh/known_hosts <<EOF
-                git.schwem.io ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMjkwVwr1iP35frNmaaB+eW63+m8ExKtQ5OA8qen4dz0
-                EOF
-
-                cat > ~/.netrc <<EOF
-                  machine $REPO_HOST
-                    login $REPO_USER
-                    password $REPO_PASS
-                EOF
-              '';
-
-            GOPRIVATE = "git.schwem.io";
-          };
+      perSystem = {
+        config,
+        pkgs,
+        system,
+        ...
+      }: {
+        # makes pkgs available to all perSystem functions
+        _module.args.pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
         };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            age
+            air
+            go
+            nodejs
+            sqlc
+            sops
+            tailwindcss
+            templ
+          ];
+
+          shellHook = ''
+            export GOPRIVATE=git.schwem.io
+          '';
+        };
+
+        packages.default = pkgs.buildGo123Module {
+          name = "oidcsso";
+
+          src = ./.;
+          vendorHash = "sha256-O7A0nQZuAjtiCE6bNuMpmorRUfksKz6lwt9n5IlzRwQ=";
+          preBuild =
+            /*
+            bash
+            */
+            ''
+              export HOME=$(mktemp -d)
+              export NIX_SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt
+              export SOPS_AGE_KEY_FILE=/etc/sops/age-keys.txt
+
+              source <(${pkgs.sops}/bin/sops -d ${./build.env})
+              echo $REPO_HOST
+
+              mkdir -p ~/.ssh/
+
+              cat > ~/.ssh/known_hosts <<EOF
+              git.schwem.io ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMjkwVwr1iP35frNmaaB+eW63+m8ExKtQ5OA8qen4dz0
+              EOF
+
+              cat > ~/.netrc <<EOF
+                machine $REPO_HOST
+                  login $REPO_USER
+                  password $REPO_PASS
+              EOF
+            '';
+
+          GOPRIVATE = "git.schwem.io";
+        };
+      };
     };
 }
